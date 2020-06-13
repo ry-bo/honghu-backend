e4a4d5c8bcdf692f5f9bb6e2f917fdd052aa2c7d
riiy zhang <riiy.zhang@gmail.com>
2020-06-13 19:46:19 +0800

    添加sentry’

--- honghu/config/common.py
+++ honghu/config/common.py
@@ -5,7 +5,7 @@ import dj_database_url
 from configurations import Configuration
 from dotenv import load_dotenv
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-load_dotenv(os.path.join(BASE_DIR, '.env'))
+load_dotenv(os.path.join(BASE_DIR, '../.env'))
 
 class Common(Configuration):
 
--- honghu/config/production.py
+++ honghu/config/production.py
@@ -1,6 +1,16 @@
 import os
 from .common import Common
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
 
+sentry_sdk.init(
+    dsn=os.getenv('SENTRY_DSN', ""),
+    integrations=[DjangoIntegration()],
+
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True
+)
 
 class Production(Common):
     INSTALLED_APPS = Common.INSTALLED_APPS
