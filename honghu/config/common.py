62c0a0436e0d7aa6b010e3866b15327dab6dd223
riiy zhang <riiy.zhang@gmail.com>
2020-06-13 17:56:44 +0800

    添加loadenv

--- honghu/config/common.py
+++ honghu/config/common.py
@@ -3,8 +3,9 @@ from os.path import join
 from distutils.util import strtobool
 import dj_database_url
 from configurations import Configuration
+from dotenv import load_dotenv
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+load_dotenv(os.path.join(BASE_DIR, '.env'))
 
 class Common(Configuration):
 
