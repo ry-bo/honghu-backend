febf74988e58d1208435f7d4834dc343819eb13e
riiy zhang <riiy.zhang@gmail.com>
2020-06-30 20:04:21 +0800

    去掉权限

--- honghu/feeds/views.py
+++ honghu/feeds/views.py
@@ -1,5 +1,7 @@
-from rest_framework import generics
 from photologue.models import Photo
+from rest_framework import generics
+from rest_framework.permissions import AllowAny
+
 from .serializers import HotPicsSerializer
 
 
@@ -7,3 +9,4 @@ class HotPicsAPIView(generics.ListAPIView):
     ''''''
     queryset = Photo.objects.all()
     serializer_class = HotPicsSerializer
+    permission_classes = [AllowAny, ]
