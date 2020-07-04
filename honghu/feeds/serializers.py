135df9d705ecfd9f3cdf5eef8d3052d7d413af05
riiy zhang <riiy.zhang@gmail.com>
2020-07-04 21:24:49 +0800

    add detail

--- honghu/feeds/serializers.py
+++ honghu/feeds/serializers.py
@@ -1,7 +1,11 @@
 from rest_framework import serializers
 from photologue.models import Photo
 
-class HotPicsSerializer(serializers.ModelSerializer):
+class PicSerializer(serializers.ModelSerializer):
     class Meta:
         model = Photo
         exclude = ['sites', ]
+class PicsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Photo
+        fields = ['id', 'image', 'title']
