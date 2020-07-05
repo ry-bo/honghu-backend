282e1d3be0ba8cba1a44347721c8b26ae48eab47
riiy zhang <riiy.zhang@gmail.com>
2020-07-05 12:53:12 +0800

    只显示is_public

--- honghu/feeds/serializers.py
+++ honghu/feeds/serializers.py
@@ -1,11 +1,18 @@
 from rest_framework import serializers
 from photologue.models import Photo
 
+
 class PicSerializer(serializers.ModelSerializer):
     class Meta:
         model = Photo
-        exclude = ['sites', ]
+        exclude = [
+            'sites',
+        ]
+
+
 class PicsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Photo
-        fields = ['id', 'image', 'title', 'view_count', 'crop_from', 'date_added']
+        fields = [
+            'id', 'image', 'title', 'view_count', 'crop_from', 'date_added'
+        ]
--- honghu/feeds/views.py
+++ honghu/feeds/views.py
@@ -1,35 +1,44 @@
-from photologue.models import Photo
 from rest_framework import generics
 from rest_framework.permissions import AllowAny
+from photologue.models import Photo
+
 
 from .serializers import PicsSerializer, PicSerializer
 
 
 class NewPicsAPIView(generics.ListAPIView):
-    ''''''
-    queryset = Photo.objects.all()
+    '''新图速递'''
+    queryset = Photo.objects.filter(is_public=True).all()
     serializer_class = PicsSerializer
-    permission_classes = [AllowAny, ]
+    permission_classes = [
+        AllowAny,
+    ]
 
 
 class RecommandPicsAPIView(generics.ListAPIView):
     ''''''
-    queryset = Photo.objects.order_by('?')[:100]
+    queryset = Photo.objects.filter(is_public=True).order_by('?')[:100]
     serializer_class = PicsSerializer
-    permission_classes = [AllowAny, ]
+    permission_classes = [
+        AllowAny,
+    ]
 
 
 class HotPicsAPIView(generics.ListAPIView):
     ''''''
-    queryset = Photo.objects.order_by('-view_count')
+    queryset = Photo.objects.filter(is_public=True).order_by('-view_count')
     serializer_class = PicsSerializer
-    permission_classes = [AllowAny, ]
+    permission_classes = [
+        AllowAny,
+    ]
 
 
 class PicDetailAPIView(generics.RetrieveAPIView):
     serializer_class = PicSerializer
     queryset = Photo.objects.all()
-    permission_classes = [AllowAny, ]
+    permission_classes = [
+        AllowAny,
+    ]
 
     def get_object(self):
         object = super().get_object()
